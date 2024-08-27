743c626e2c66f09c6dc99a84d9f227bbf1f009d4
Michael <michaliskarag@gmail.com>
2024-08-27 17:17:30 +0300

    Update swiftarchdiagram.rb

--- Formula/swiftarchdiagram.rb
+++ Formula/swiftarchdiagram.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Swiftarchdiagram < Formula
+class SwiftArchDiagram < Formula
   desc "A command line tool that generates mermaid diagram for Swift Packages"
   homepage "https://github.com/michaelversus/SwiftArchDiagram"
   url "https://github.com/michaelversus/SwiftArchDiagram"
@@ -12,22 +12,10 @@ class Swiftarchdiagram < Formula
   # depends_on "cmake" => :build
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "SwiftArchDiagram"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test SwiftArchDiagram`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    system "#{bin}/SwiftArchDiagram", "--version"
   end
 end
