c4bdc8b1a9cc8cabf47facf422bc4a5237c99962
Michael <michaliskarag@gmail.com>
2024-08-27 18:36:43 +0300

    Update swiftarchdiagram.rb

--- swiftarchdiagram.rb
+++ swiftarchdiagram.rb
@@ -2,7 +2,7 @@ class Swiftarchdiagram < Formula
   desc "This is a command-line tool that generates mermaid diagram for Swift Packages"
   homepage "https://github.com/michaelversus/SwiftArchDiagram"
   url "https://github.com/michaelversus/SwiftArchDiagram"
-  version "0.1.0"
+  version "0.1.1"
   sha256 "66e8f0bab44097f28a9049b1f1b228c4a3ac3c583ebf3ceee95d30a921fe3d54"
   license "MIT"
   uses_from_macos "swift"
