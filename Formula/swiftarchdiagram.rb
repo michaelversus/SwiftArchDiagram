99e17bac255ffd7ba3770b9a737f6a404b80ae37
Michael <michaliskarag@gmail.com>
2024-08-27 17:17:53 +0300

    Update swiftarchdiagram.rb

--- Formula/swiftarchdiagram.rb
+++ Formula/swiftarchdiagram.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SwiftArchDiagram < Formula
   desc "A command line tool that generates mermaid diagram for Swift Packages"
   homepage "https://github.com/michaelversus/SwiftArchDiagram"
@@ -8,8 +5,6 @@ class SwiftArchDiagram < Formula
   version "0.1.0"
   sha256 "5b1c2d8089125e839339690a82018fbd9cb2962a5ee13a6f8b2dbde68f4fb393"
   license ""
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "SwiftArchDiagram"
